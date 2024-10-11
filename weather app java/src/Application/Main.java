aea1ab58eafc2f02acf039eefd91847a9dd374b3
scosme20 <scosmeagostinho@hotmail.com>
2024-10-11 09:26:40 -0300

    first commit

---
+++ weather app java/src/Application/Main.java
@@ -0,0 +1,17 @@
+package Application;
+import javax.swing.*;
+public class Main extends JFrame {
+    public Main(){
+        super("Weather App");
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setSize(450, 650);
+
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+
+        setResizable(false);
+    }
+}
